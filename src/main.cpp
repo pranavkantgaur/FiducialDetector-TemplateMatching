c36e194d1fa671ddcb5e4d93f74a8000cbc598e3
pranavkantgaur <pranav8iisc@gmail.com>
2017-11-20 09:41:13 +0530

    ITK DICOM reading supported

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 	if (argc < 2)
 	{
 		std::cerr << "Usage: " << std::endl;
-		std::cerr << argv[0] << "[Detector Configuration File name]";
+		std::cerr << argv[0] << "[Detector Configuration File name]" << endl;
 		exit(1);	
 	}
 	FiducialDetection detector;
