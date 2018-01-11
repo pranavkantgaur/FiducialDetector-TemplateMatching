6b7d91e84b6393dbcf2b7fd71404036bd9adc853
pranavkantgaur <pranav8iisc@gmail.com>
2018-01-11 15:35:24 +0530

    removes redundant declarations and matches function signature

--- include/SpectralClusteringTemplateSelection.h
+++ include/SpectralClusteringTemplateSelection.h
@@ -2,13 +2,12 @@
  * \brief The class implements the spectral clustering approach for template selection.
 */
 #include <vector>
+#include <itkArray.h>
 #include <itkAffinityClustering.h>
+#include "Globals.h"
 
 class SpectralClusteringTemplateSelection{
-	private:
-		std::vector<std::vector<float> > affinityMatrix;
-		unsigned int affinityMatrixSize;
 	public:
-		void generateFiducialTemplates(float, std::vector<int>&);		
+		void generateFiducialTemplates(float, std::vector<FiducialOrientation>&, itk::Array<unsigned int>&);		
 
 };
